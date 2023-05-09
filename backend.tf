e32adc8167a91921d20d05aa7886be4cc8bd5fab
ZephyrDavid <99273426+ZephyrDavid@users.noreply.github.com>
2023-05-09 23:19:51 +0100

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "my-own-dev-tf-state-bucket"
-    key = "LockID"
+    key = "myJenkinsKey"
     region = "us-east-1"
     dynamodb_table = "my-dynamodb-table"
   }
