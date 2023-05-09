3189db2c86ddc47507340069f2662cf1bb73d1c4
ZephyrDavid <99273426+ZephyrDavid@users.noreply.github.com>
2023-05-09 21:24:43 +0100

    Updated variables.tf

--- variables.tf
+++ variables.tf
@@ -24,7 +24,7 @@ variable "tag_name" {
 } 
 variable "ami_id" { 
     description = "AMI for Ubuntu Ec2 instance" 
-    default     = "ami-0ee23bfc74a881de5" 
+    default     = "ami-007855ac798b5175e" 
 }
 variable "versioning" {
     type        = bool
@@ -39,7 +39,7 @@ variable "acl" {
 variable "bucket_prefix" {
     type        = string
     description = "(required since we are not using 'bucket') Creates a unique bucket name beginning with the specified prefix"
-    default     = "my-s3bucket-"
+    default     = "my-own-dev-tf-state-bucket"
 }
 variable "tags" {
     type        = map
