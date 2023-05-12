8199334362aa708f6c6b1bdc92b670d735b009d8
ZephyrDavid <99273426+ZephyrDavid@users.noreply.github.com>
2023-05-12 17:31:40 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ resource "aws_vpc" "main" {
 
 #Create security group with firewall rules
 resource "aws_security_group" "jenkins-sg-2022" {
-  name        = var.sg-0e12f567e1eaf87f7
+  name        = var.security_group
   description = "security group for Ec2 instance"
 
   ingress {
